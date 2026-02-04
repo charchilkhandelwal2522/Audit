9f9f0488575a7769b1fbd36e43823860c76dc911
Charchil <charchil@froiden.com>
2026-02-04 12:56:18 +0530

    WIP

--- Resources/views/audits/pdf/report.blade.php
+++ Resources/views/audits/pdf/report.blade.php
@@ -262,7 +262,7 @@
                                     <div class="file-item">
                                         <span class="file-icon">📎</span>
                                         <a href="{{ $file->file_url }}" class="file-link" target="_blank">
-                                            {{-- {{ $file->filename }} --}}
+                                            {{ $file->filename }}
                                         </a>
                                         <span class="file-size">({{ $file->formatted_size }})</span>
                                     </div>
